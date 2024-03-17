4150e81bf1497e18eee048f27143f60f81a2afd2
venkatsaipalla <venkatasaipalla2019@email.com>
2024-03-17 17:51:58 +0530

    firebase fixed

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -1,24 +1,15 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration - Copied from FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyBGnqwsY-hjaNQi9AomheVARiJ5IoCvyRw",
-
-  authDomain: "listedassignment-62fcf.firebaseapp.com",
-
-  projectId: "listedassignment-62fcf",
-
-  storageBucket: "listedassignment-62fcf.appspot.com",
-
-  messagingSenderId: "970823432142",
-
-  appId: "1:970823432142:web:f3831f03ca74e427a3dd9f",
-
-  measurementId: "G-4PLNBZV10K"
-
+  apiKey: "AIzaSyDZmfBXuZfPpEVRiQJjGuOwxgVImfbm67o",
+  authDomain: "open-in-app-d0e1b.firebaseapp.com",
+  projectId: "open-in-app-d0e1b",
+  storageBucket: "open-in-app-d0e1b.appspot.com",
+  messagingSenderId: "763076028405",
+  appId: "1:763076028405:web:d6c5174c0f4eab03e462bc",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
